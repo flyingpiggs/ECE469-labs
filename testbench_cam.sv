870da4d51a8692937859b84bc5c047a3d054b467
flyingpiggs <chen172@uic.edu>
2020-04-03 17:26:12 -0500

    Wrote testbench for Hamming weight counter

--- testbench_cam.sv
+++ testbench_cam.sv
@@ -1,1 +1,18 @@
+module testbench_HammingWeight();
 
+	logic[7:0] value;
+	logic[3:0] count; 
+	HammingWeight_8bit duts( value, count );
+
+	initial begin
+		value = 8'b00000000; #10;
+		value = 8'b00000001; #10;
+		value = 8'b00000011; #10;
+		value = 8'b00000111; #10;
+		value = 8'b00001111; #10;
+		value = 8'b00011111; #10;
+		value = 8'b00111111; #10;
+		value = 8'b01111111; #10;
+		value = 8'b11111111; #10;
+	end  
+endmodule 
