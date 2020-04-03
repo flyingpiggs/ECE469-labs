2da871b156a4511e5ec508260efea1882fe39f0f
flyingpiggs <chen172@uic.edu>
2020-04-03 15:50:33 -0500

    Added in Jessica's top

--- mipstop.sv
+++ mipstop.sv
@@ -2,18 +2,30 @@
 // Top level system including MIPS and memories
 //------------------------------------------------
 
-module top(input  logic       clk, reset, 
+/*module top(input  logic       clk, reset, 
            output logic [31:0] writedata, dataadr, 
-           output logic       memwrite);
+           output logic       memwrite);*/
 
-  logic [31:0] pc, instr, readdata;
+module top(input  logic        clk, reset,
+            output logic [31:0] pc,
+            output logic [31:0] instr,
+            output logic        memwrite,
+            output logic [31:0] aluout, aluExt,/*------------------ADDED ALUEXT*/
+	    output logic [31:0] writedata,
+            output logic [31:0] readdata);
+
+  logic [31:0] Pc, Instr, Readdata, Aluout;
   
   // instantiate processor and memories
 
-  mips mips(clk, reset, pc, instr, memwrite, dataadr, writedata, readdata);
+  mips mips(clk, reset, Pc, Instr, memwrite, Aluout, /*dataadr,*/ writedata, aluExt, Readdata);/*------------------------ADDED ALUEXT*/
 
-  imem imem(pc[7:2], instr);
+  imem imem(Pc[7:2], Instr);
 
-  dmem dmem(clk, memwrite, dataadr, writedata, readdata);
+  dmem dmem(clk, memwrite, Aluout, /*dataadr,*/ writedata, Readdata);
 
+  assign pc = Pc;
+  assign instr = Instr;
+  assign readdata = Readdata;
+  assign aluout = Aluout;
 endmodule
