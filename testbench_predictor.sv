9fca48c8b362b555291986aa4877e0d76c9cb03d
jesv2 <12jes.v@gmail.com>
2020-04-10 19:19:55 -0500

    New testbench

--- testbench_predictor.sv
+++ testbench_predictor.sv
@@ -1,8 +1,13 @@
 module testbench_predictor();
 
-logic clk, reset, actual_pattern, predicted_pattern;
+logic clk, reset, X;
+logic [7:0] X_cnt;
+logic Y, Y_match;
+logic [7:0] Y_cnt;
+logic Z, Z_match;
+logic [7:0] Z_cnt;
 
-pattern_predictor_2bit dut(clk, reset, actual_pattern, predicted_pattern);
+PatternPredictor dut(clk, reset, X, X_cnt, Y, Y_match, Y_cnt, Z, Z_match, Z_cnt);
 
 always
 	begin
@@ -12,14 +17,13 @@ always
 
 //Pattern is 000011111111000011110000000011110000
 initial begin
-	reset = 1; #10;
-	reset = 0; 
-	actual_pattern = 0;#40;
-	actual_pattern = 1;#80;
-	actual_pattern = 0;#40;
-	actual_pattern = 1;#40;
-	actual_pattern = 0;#80;
-	actual_pattern = 1;#40;
-	actual_pattern = 0;#40;
+	reset = 1; X = 0;#10; 
+	reset = 0; X = 0;#30;
+	X = 1;#80;
+	X = 0;#40;
+	X = 1;#40;
+	X = 0;#80;
+	X = 1;#40;
+	X = 0;#40;
 	end
 endmodule
