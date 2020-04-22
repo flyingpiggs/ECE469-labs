96032285a9a1f93ba9959a7619fc477d3fd47426
jesv2 <12jes.v@gmail.com>
2020-04-21 19:27:21 -0500

    Still working on adding ALU, etc.

--- mipsmulti.sv
+++ mipsmulti.sv
@@ -220,7 +220,26 @@ module datapath(input  logic        clk, reset,
 */   
   /* ADD DATAPATH CODE HERE */  
 
- endmodule 
+endmodule 
+
+module aludec(input logic [5:0] funct,
+	input logic [1:0] aluop,
+	output logic [2:0] alucontrol);
+	
+	always_comb
+		case(aluop)
+			2'b00: alucontrol <= 3'b010; // add (for lw/sw/addi)
+			2'b01: alucontrol <= 3'b110; // sub (for beq)
+			default: case(funct) // R-type instructions
+				6'b100000: alucontrol <= 3'b010; // add
+				6'b100010: alucontrol <= 3'b110; // sub
+				6'b100100: alucontrol <= 3'b000; // and
+				6'b100101: alucontrol <= 3'b001; // or
+				6'b101010: alucontrol <= 3'b111; // slt
+				default: alucontrol <= 3'bxxx; // ???
+				endcase
+		endcase
+endmodule
 
 module mux3 #(parameter WIDTH = 8)
               (input  logic [WIDTH-1:0] d0, d1, d2,
@@ -228,7 +247,7 @@ module mux3 #(parameter WIDTH = 8)
                output logic [WIDTH-1:0] y);
 
    assign #1 y = s[1] ? d2 : (s[0] ? d1 : d0);
- endmodule
+endmodule
 
 module mux4 #(parameter WIDTH = 8)
               (input  logic [WIDTH-1:0] d0, d1, d2, d3,
