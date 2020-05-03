643a3558765b697d7e8367378a54009b5cc72a14
jesv2 <12jes.v@gmail.com>
2020-05-02 21:31:51 -0500

    Added other mips parts/modules for the datapath

--- mipsmulti.sv
+++ mipsmulti.sv
@@ -315,26 +315,6 @@ module datapath(input  logic        clk, reset,
 
 endmodule
 
-//Regfile provided in the textbook------------------------
-module regfile(input logic clk,
-input logic we3,
-input logic [4:0] ra1, ra2, wa3,
-input logic [31:0] wd3,
-output logic [31:0] rd1, rd2);
-logic [31:0] rf[31:0];
-// three ported register file
-// read two ports combinationally
-// write third port on rising edge of clk
-// register 0 hardwired to 0
-// note: for pipelined processor, write third port
-// on falling edge of clk
-always_ff @(posedge clk)
-if (we3) rf[wa3] <= wd3;
-assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
-assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
-endmodule
-//--------------------------------------------------------
-
 //ALU Portion---------------------------------------------
 module alu(input logic [31:0] a, b,
  input logic [2:0] f,
@@ -640,3 +620,68 @@ module mux4 #(parameter WIDTH = 8)
           2'b11: y = d3;
        endcase
  endmodule
+
+ // Components used in MIPS processor----------------------------------------------------------
+
+module regfile(input  logic        clk, we3, 
+               input  logic [4:0]  ra1, ra2, wa3, 
+               input  logic [31:0] wd3, 
+               output logic [31:0] rd1, rd2);
+
+  logic [31:0] rf[31:0];
+
+  // three ported register file with register 0 hardwired to 0
+  // read two ports combinationally; write third port on rising edge of clock
+
+  always_ff @(posedge clk)
+    if (we3) rf[wa3] <= wd3;	
+
+  assign rd1 = (ra1 != 0) ? rf[ra1] : 0;
+  assign rd2 = (ra2 != 0) ? rf[ra2] : 0;
+endmodule
+
+module adder(input  logic [31:0] a, b,
+             output logic [31:0] y);
+
+  assign y = a + b;
+endmodule
+
+module sl2(input  logic [31:0] a,
+           output logic [31:0] y);
+
+  assign y = {a[29:0], 2'b00}; 		// shift left by 2
+endmodule
+
+module signext(input  logic [15:0] a,
+               output logic [31:0] y);
+              
+  assign y = {{16{a[15]}}, a};
+endmodule
+
+module flopr #(parameter WIDTH = 8)
+              (input  logic             clk, reset,
+               input  logic [WIDTH-1:0] d, 
+               output logic [WIDTH-1:0] q);
+
+  always_ff @(posedge clk, posedge reset)
+    if (reset) q <= 0;
+    else       q <= d;
+endmodule
+
+module flopenr #(parameter WIDTH = 8)
+                (input  logic             clk, reset, en,
+                 input  logic [WIDTH-1:0] d, 
+                 output logic [WIDTH-1:0] q);
+ 
+  always_ff @(posedge clk, posedge reset)
+    if      (reset) q <= 0;
+    else if (en)    q <= d;
+endmodule
+
+module mux2 #(parameter WIDTH = 8)
+             (input  logic [WIDTH-1:0] d0, d1, 
+              input  logic             s, 
+              output logic [WIDTH-1:0] y);
+
+  assign y = s ? d1 : d0; 
+endmodule
